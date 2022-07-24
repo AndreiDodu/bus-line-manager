05ed1369ed15dfb102e9e9d784ef2dc3db9a009c
Andrei Dodu <andrei.dodu@hotmail.it>
2022-07-24 13:14:27 +0200

    moved dozer config in config package

---
+++ src/main/java/com/andreidodu/blm/config/DozerConfig.java
@@ -0,0 +1,20 @@
+package com.andreidodu.blm.config;
+
+import java.io.IOException;
+
+import org.dozer.spring.DozerBeanMapperFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+@Configuration
+public class DozerConfig {
+	@Bean
+	public DozerBeanMapperFactoryBean dozerMapper(ResourcePatternResolver resourcePatternResolver) throws IOException {
+		DozerBeanMapperFactoryBean factoryBean = new DozerBeanMapperFactoryBean();
+//        factoryBean.setMappingFiles(
+//            resourcePatternResolver.getResources("classpath*:/*mapping.xml"));
+
+		return factoryBean;
+	}
+}
